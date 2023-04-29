2e9b6c034e44779847d27bd0a2c7189a7cf5f6b2
David <david.de.schrijver@gmail.com>
2023-04-29 17:57:25 +0200

    Egg jiggling physics !

--- Assets/Scripts/EggController.cs
+++ Assets/Scripts/EggController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class EggController : MonoBehaviour
@@ -6,23 +7,47 @@ public class EggController : MonoBehaviour
     
     private Rigidbody2D rigidBody;
     private Vector2 direction;
+    private bool collidesWithPlayer;
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
-        var horizontalAxis = Input.GetAxis("Horizontal");
-        direction = new Vector2(horizontalAxis, 1).normalized;
-        
-        if (Input.GetKeyDown(KeyCode.E))
+        if (collidesWithPlayer && Input.GetKeyDown(KeyCode.E))
         {
+            var horizontalAxis = Input.GetAxis("Horizontal");
+            direction = new Vector2(horizontalAxis, 1).normalized;
+            
             rigidBody.velocity = Vector2.zero;
             rigidBody.angularVelocity = 0f;
             rigidBody.AddForce(direction * jumpPower, ForceMode2D.Impulse);
             rigidBody.AddTorque(10f);
+            collidesWithPlayer = false;
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collidesWithPlayer = true;
+        }
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.angularVelocity = 0f;
+            rigidBody.bodyType = RigidbodyType2D.Static;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            collidesWithPlayer = false;
+        }    
     }
 }
