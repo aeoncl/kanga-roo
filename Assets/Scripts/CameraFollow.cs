f31de1685173894eddcdc8738a53dfee23fc85f3
David <david.de.schrijver@gmail.com>
2023-04-30 19:45:23 +0200

    Add parallax effect on background image

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,37 +6,32 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform followTransform;
-
     public Transform eggTransform;
+    public float eggUpperLimitFollow = 4;
+    public float parallaxSpeed = 1f;
 
     private float initialY;
+    private SpriteRenderer _backGroundSprite;
+    private float _lastKnownXPosition;
 
-    public float eggUpperLimitFollow = 4;
-
-    // Start is called before the first frame update
     void Start()
     {
        this.initialY = this.transform.position.y;
+       this._backGroundSprite = transform.GetComponentInChildren<SpriteRenderer>();
+       this._lastKnownXPosition = followTransform.position.x;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /// <summary>
-    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
-    /// </summary>
     void FixedUpdate()
     {
         if(eggTransform.position.y >= (this.initialY + this.eggUpperLimitFollow)) {
-        var test = Vector3.Lerp(this.transform.position, eggTransform.position - new Vector3(0,3.5f,0), Time.fixedDeltaTime * 2);
-        this.transform.position = new Vector3(followTransform.position.x, test.y, this.transform.position.z);
+            var test = Vector3.Lerp(this.transform.position, eggTransform.position - new Vector3(0,3.5f,0), Time.fixedDeltaTime * 2);
+            this.transform.position = new Vector3(followTransform.position.x, test.y, this.transform.position.z);
         } else {
             var initialYLerp = Mathf.Lerp(this.transform.position.y, this.initialY, Time.fixedDeltaTime * 2);
             this.transform.position = new Vector3(followTransform.position.x, initialYLerp, this.transform.position.z);
         }
-        
+
+        _backGroundSprite.material.mainTextureOffset += new Vector2(followTransform.position.x - _lastKnownXPosition, 0f) / 500;
+        _lastKnownXPosition = followTransform.position.x;
     }
 }
