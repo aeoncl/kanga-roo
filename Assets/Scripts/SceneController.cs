d43303f4a2266a6f74250c0d95a9b77a98ed9f28
David <david.de.schrijver@gmail.com>
2023-04-30 12:30:27 +0200

    Main menu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController _instance;
+
+    public static SceneController Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void LoadFirstScene()
+    {
+        SceneManager.LoadScene("EggMovementPOC");
+    }
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+    }
+}
