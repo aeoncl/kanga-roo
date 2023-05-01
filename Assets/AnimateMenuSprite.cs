0893e4646d7626bc91bbf5caa3126d6b8ddd8f63
David <david.de.schrijver@gmail.com>
2023-05-01 14:29:08 +0200

    Fix buttons & UI misc

---
+++ Assets/AnimateMenuSprite.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimateMenuSprite : MonoBehaviour
+{
+    private Image _image;
+    void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
